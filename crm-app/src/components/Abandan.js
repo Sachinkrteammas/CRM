273b26b4dcdd4403e47923520ec4d6ea5c4fe01a
sneha <sneha.kumari@teammas.co.in>
2025-04-17 16:21:20 +0530

    ok

--- crm-app/src/components/Abandan.js
+++ crm-app/src/components/Abandan.js
@@ -2,24 +2,28 @@ import React, { useState } from "react";
 import Layout from "../layout";
 import "../layout.css";
 import "./report1.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
-
-const Report3 = () => {
+const Report1 = () => {
     const [clientId, setClientId] = useState("");
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
+    const [selectId, setSelectId] = useState("");
+
+
 
     const handleExport = () => {
         alert(`Exporting report for Client ID: ${clientId} from ${startDate} to ${endDate}`);
     };
 
-
-
     return (
         <Layout>
-            <h2>OB CDR</h2>
+            <h4>Abandan Report</h4>
             <div className="report-form-container">
+
                 <div className="report-form">
+
 
                     <div className="form-group">
                         <label>Client ID</label>
@@ -32,22 +36,42 @@ const Report3 = () => {
                         />
                     </div>
 
+                    <div className="form-group-select">
+                        <label>Select</label>
+                        <select
+                            value={selectId}
+                            onChange={(e) => setSelectId(e.target.value)}
+                            className="input-box-select"
+                        >
+                            <option value="">Select</option>
+                            <option value="all">All</option>
+                            <option value="hv">HV</option>
+                            <option value="lv">LV</option>
+                            <option value="mv">MV</option>
+                        </select>
+                    </div>
+
+
                     <div className="form-group">
                         <label>Start Date</label>
-                        <input
-                            type="date"
-                            value={startDate}
-                            onChange={(e) => setStartDate(e.target.value)}
+                        <DatePicker
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            placeholderText="Start Date"
+                            // dateFormat="dd/MM/yyyy"
                             className="input-box"
                         />
                     </div>
 
+
+
                     <div className="form-group">
                         <label>End Date</label>
-                        <input
-                            type="date"
-                            value={endDate}
-                            onChange={(e) => setEndDate(e.target.value)}
+                        <DatePicker
+                            selected={endDate}
+                            onChange={(date) => setEndDate(date)}
+                            placeholderText="End Date"
+                            // dateFormat="dd/MM/yyyy"
                             className="input-box"
                         />
                     </div>
@@ -55,10 +79,13 @@ const Report3 = () => {
                     <button onClick={handleExport} className="export-btn">
                         Export
                     </button>
+                    <button className="view-btn">
+                        View
+                    </button>
                 </div>
             </div>
         </Layout>
     );
 };
 
-export default Report3;
+export default Report1;
